b6e1bd08e7d86f5da7f95f4cd9b3c4d2bbc18a9a
tentey45 <cponhvorntey@gmail.com>
2025-12-10 08:27:35 +0700

    develop: conflict resovle

--- src/api/weather.js
+++
@@ -1,9 +0,0 @@
-const API_KEY = '9e77ec7492fee5be08be9e2c4a0b6493';
-
-export async function getWeatherByCity(city) {
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
-  );
-  if (!response.ok) throw new Error("City not found");
-  return response.json();
-}
