eab1680a5fd564cf05bc051aea14492413d1d7c7
tentey45 <cponhvorntey@gmail.com>
2025-12-10 19:19:54 +0700

    update search page to default weather

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import search from './assets/search-svgrepo-com.svg'
 import notfound from './assets/notepad-svgrepo-com.svg'
@@ -6,12 +6,16 @@ import { getWeatherIcon } from "./utils/getWeatherIcon";
 import importIcon from "./utils/importIcon";
 
 const API_KEY = "9e77ec7492fee5be08be9e2c4a0b6493";
-  
+
 export default function App() {
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState(null);
   const [forecast, setForecast] = useState([]);
   const [notFound, setNotFound] = useState(false);
+
+   useEffect(() => {
+    fetchWeather("Phnom Penh"); // Default weather
+  }, []);
 
   async function fetchWeather(cityName) {
     try {
@@ -34,8 +38,8 @@ export default function App() {
 
       fetchForecast(cityName);
     } catch (err) {
-        setWeather(null);
-        setNotFound(true);
+      setWeather(null);
+      setNotFound(true);
       console.error(err);
     }
   }
@@ -81,8 +85,8 @@ export default function App() {
         </button>
       </div>
 
-      {!weather && !notFound && (
-        <section className="search-city section-message">
+
+      {/* <section className="search-city section-message">
           <img className="message-img" src={search} alt="" />
           
 
@@ -90,103 +94,104 @@ export default function App() {
             <h1>Search City</h1>
             <h4 className="regular-txt">Find out the weather condition of the city</h4>
           </div>
-        </section>
-      )}
+        </section> */}
+
+      
 
 
-      {/* Not found image */}
-      {notFound && (
-        <div className="not-found section-message">
-          <img src={notfound} alt="Not found" />
-          <h2>City not found</h2>
-        </div>
-      )}
-
-      {/* Weather card */}
-      {weather && (
-        <div className="weather-info">
-          <div className="location-date-container">
-            <div className="location">
-              <span className="material-symbols-outlined">
-                location_on
-              </span>
-              <h4 className="country-txt">{weather.name}</h4>
+          {/* Not found image */}
+          {notFound && (
+            <div className="not-found section-message">
+              <img src={notfound} alt="Not found" />
+              <h2>City not found</h2>
             </div>
-            <h5 className="current-date-txt">
-              {new Date().toLocaleDateString("en-GB", {
-                weekday: "short",
-                day: "2-digit",
-                month: "short",
-              })}
-            </h5>
-          </div>
+          )}
 
-          <div className="weather-summary-container">
-            <img
-              className="weather-summary-img"
-              src={importIcon(getWeatherIcon(weather.weather[0].id))}
-              alt="weather"
-            />
-            <div className="weather-summary-info">
-              <h1 className="temp-txt">{Math.round(weather.main.temp)} °C</h1>
-              <h3 className="condition-txt">{weather.weather[0].main}</h3>
-            </div>
-          </div>
-
-          <div className="weather-conditions-container">
-            <div className="condition-item">
-              <span className="material-symbols-outlined">water_drop</span>
-              <div className="condition-info">
-                <h5>Humidity</h5>
-                <h5>{weather.main.humidity} %</h5>
-              </div>
-            </div>
-
-            <div className="condition-item">
-              <span className="material-symbols-outlined">air</span>
-              <div className="condition-info">
-                <h5>Wind</h5>
-                <h5>{weather.wind.speed} M/s</h5>
-              </div>
-            </div>
-
-            <div className="condition-item">
-              <span class="material-symbols-outlined">
-                thermometer
-              </span>
-              <div className="condition-info">
-                <h5>Feels Like</h5>
-                <h5>{weather.main.feels_like} °C</h5>
-              </div>
-            </div>
-
-
-
-          </div>
-
-          {/* Forecast */}
-          <div className="forecast-items-container">
-            {forecast.map((item) => (
-              <div key={item.dt} className="forecast-item">
-                <h5>
-                  {new Date(item.dt_txt).toLocaleDateString("en-US", {
+          {/* Weather card */}
+          {weather && (
+            <div className="weather-info">
+              <div className="location-date-container">
+                <div className="location">
+                  <span className="material-symbols-outlined">
+                    location_on
+                  </span>
+                  <h4 className="country-txt">{weather.name}</h4>
+                </div>
+                <h5 className="current-date-txt">
+                  {new Date().toLocaleDateString("en-GB", {
+                    weekday: "short",
                     day: "2-digit",
                     month: "short",
                   })}
                 </h5>
-
-                <img
-                  src={importIcon(getWeatherIcon(item.weather[0].id))}
-                  className="forecast-item-img"
-                  alt=""
-                />
-
-                <h5>{Math.round(item.main.temp)} °C</h5>
               </div>
-            ))}
-          </div>
+
+              <div className="weather-summary-container">
+                <img
+                  className="weather-summary-img"
+                  src={importIcon(getWeatherIcon(weather.weather[0].id))}
+                  alt="weather"
+                />
+                <div className="weather-summary-info">
+                  <h1 className="temp-txt">{Math.round(weather.main.temp)} °C</h1>
+                  <h3 className="condition-txt">{weather.weather[0].main}</h3>
+                </div>
+              </div>
+
+              <div className="weather-conditions-container">
+                <div className="condition-item">
+                  <span className="material-symbols-outlined">water_drop</span>
+                  <div className="condition-info">
+                    <h5>Humidity</h5>
+                    <h5>{weather.main.humidity} %</h5>
+                  </div>
+                </div>
+
+                <div className="condition-item">
+                  <span className="material-symbols-outlined">air</span>
+                  <div className="condition-info">
+                    <h5>Wind</h5>
+                    <h5>{weather.wind.speed} M/s</h5>
+                  </div>
+                </div>
+
+                <div className="condition-item">
+                  <span class="material-symbols-outlined">
+                    thermometer
+                  </span>
+                  <div className="condition-info">
+                    <h5>Feels Like</h5>
+                    <h5>{weather.main.feels_like} °C</h5>
+                  </div>
+                </div>
+
+
+
+              </div>
+
+              {/* Forecast */}
+              <div className="forecast-items-container">
+                {forecast.map((item) => (
+                  <div key={item.dt} className="forecast-item">
+                    <h5>
+                      {new Date(item.dt_txt).toLocaleDateString("en-US", {
+                        day: "2-digit",
+                        month: "short",
+                      })}
+                    </h5>
+
+                    <img
+                      src={importIcon(getWeatherIcon(item.weather[0].id))}
+                      className="forecast-item-img"
+                      alt=""
+                    />
+
+                    <h5>{Math.round(item.main.temp)} °C</h5>
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
         </div>
-      )}
-    </div>
-  );
+      );
 }  
